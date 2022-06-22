878702396abf74de58cd87e77bc8cd2af75924be
aalapandr22 <aalap73@mail.ru>
2022-06-22 20:09:15 +0300

    Glavnoe menu

--- app/src/main/java/com/example/searchfilm1/MainActivity.kt
+++ app/src/main/java/com/example/searchfilm1/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.searchfilm1
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+//buttons
+
+        button_menu.setOnClickListener {
+            Toast.makeText(this, "МЕНЮ", Toast.LENGTH_SHORT).show()
+        }
+        button_fav.setOnClickListener {
+            Toast.makeText(this, "ИЗБРАННОЕ", Toast.LENGTH_SHORT).show()
+        }
+        button_watch_later.setOnClickListener {
+            Toast.makeText(this, "ПОСМОТРЕТЬ ПОЗЖЕ", Toast.LENGTH_SHORT).show()
+        }
+        button_selections.setOnClickListener {
+            Toast.makeText(this, "ПОДБОРКИ", Toast.LENGTH_SHORT).show()
+        }
+        button_settings.setOnClickListener {
+            Toast.makeText(this, "НАСТРОЙКИ", Toast.LENGTH_SHORT).show()
+        }
+        //
+        Toast.makeText(this, "gggg", Toast.LENGTH_SHORT).show()
     }
 }
